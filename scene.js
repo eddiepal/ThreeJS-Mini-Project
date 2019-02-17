4bea4279b21cdadcc56b38d205fc259621b1e422
eddie <empburke@outlook.com>
2019-02-17 11:39:38 +0100

    Added windows to the building and textured them and the door, next is the roof and the texturing of the wall

--- scene.js
+++ scene.js
@@ -35,12 +35,13 @@ loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
 // Create geometry
 var buildingWallGeo = new THREE.BoxGeometry(40, 40, 1);
 var buildingDoorGeo = new THREE.BoxGeometry(5,8,1);
+var buildingWindowGeo = new THREE.BoxGeometry(4,4,.2);
 var groundGeo = new THREE.PlaneGeometry(100, 100);
 var carTireGeo = new THREE.TorusGeometry(.7, 0.30, 4, 12);
 var carBaseGeo = new THREE.BoxGeometry(9, 1.6, 3.5);
 var carTopGeo = new THREE.BoxGeometry(5, 3, 2.8);
 var carConeGeo = new THREE.ConeGeometry(1, 2.5, 32);
-var carSlantGeo = new THREE.TetrahedronGeometry(3,0);
+var buildingWindowPillar = new THREE.CylinderGeometry( 5, 5, 20, 32 );
 
 
 // Create materials
@@ -49,17 +50,19 @@ var groundMaterial = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadText
 var carTireMaterial = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('textures/tiretexture.jpg')});
 var carBaseMaterial = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('textures/randomtexture.jpg')});
 var carConeMaterial = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('textures/conetexture.jpg')});
+var buildingDoorMaterial = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('textures/doortexture.jpg')});
+var buildingWindowMaterial = new THREE.MeshBasicMaterial({map: THREE.ImageUtils.loadTexture('textures/windowtexture.jpg')});
 
 
 // Combine materials and geometry (mesh)
 var buildingWall = new THREE.Mesh(buildingWallGeo, buildingWallMaterial);
-var buildingDoor = new THREE.Mesh(buildingDoorGeo, buildingWallMaterial);
+var buildingDoor = new THREE.Mesh(buildingDoorGeo, buildingDoorMaterial);
+var buildingWindow = new THREE.Mesh(buildingWindowGeo, buildingWindowMaterial);
 var ground = new THREE.Mesh(groundGeo, groundMaterial);
 var carTire1 = new THREE.Mesh(carTireGeo, carTireMaterial);
 var carBase = new THREE.Mesh(carBaseGeo, carBaseMaterial);
 var carTop = new THREE.Mesh(carTopGeo, carBaseMaterial);
 var carCone = new THREE.Mesh(carConeGeo, carConeMaterial);
-var carSlant = new THREE.Mesh(carSlantGeo, carConeMaterial);
 
 
 // Clone mesh objects
@@ -69,6 +72,9 @@ var carTire4 = carTire1.clone();
 var buildingWallLeft = buildingWall.clone();
 var buildingWallRight = buildingWall.clone();
 var buildingWallBack = buildingWall.clone();
+var buildingWindow2 = buildingWindow.clone();
+var buildingWindow3 = buildingWindow.clone();
+var buildingWindow4 = buildingWindow.clone();
 
 
 // Create groups
@@ -86,7 +92,6 @@ carGroup.add(carTire4);
 carGroup.add(carBase);
 carGroup.add(carTop);
 carGroup.add(carCone);
-carGroup.add(carSlant);
 
 var buildingGroup = new THREE.Group();
 buildingGroup.add(buildingWall);
@@ -94,7 +99,10 @@ buildingGroup.add(buildingWallLeft);
 buildingGroup.add(buildingWallRight);
 buildingGroup.add(buildingWallBack);
 buildingGroup.add(buildingDoor);
-
+buildingGroup.add(buildingWindow);
+buildingGroup.add(buildingWindow2);
+buildingGroup.add(buildingWindow3);
+buildingGroup.add(buildingWindow4);
 
 // Update positions
 //buildingWall.position.set(10, 5, 0);
@@ -105,21 +113,23 @@ carTire4.position.set(7, 1, 4);
 carBase.position.set(3.5, 1.2, 2);
 carTop.position.set(3.5, 2, 2);
 carCone.position.set(3.5, 4.8, 2);
-carSlant.position.set(5,5,5);
 carGroup.position.set(8, 0, -10);
 
 buildingWall.position.set(0, 20, 0);
 buildingWallLeft.position.set(20,20,20);
 buildingWallRight.position.set(-20,20,20);
 buildingWallBack.position.set(0,20,40);
-buildingDoor.position.set(0,4,-1);
+buildingDoor.position.set(0,4,-.3);
+buildingWindow.position.set(-10,20,-0.7);
+buildingWindow2.position.set(10,20,-0.7);
+buildingWindow3.position.set(-10,34,-0.7);
+buildingWindow4.position.set(10,34,-0.7);
 
 
 // Rotate objects
 groundGeo.rotateX(-Math.PI / 2);
 buildingWallLeft.rotateY(-Math.PI / 2);
 buildingWallRight.rotateY(-Math.PI / 2);
-carSlant.rotateX(-Math.PI / 2);
 
 
 // Add objects and groups to scene
